b076342fa36f239bccfcc9614f07e547aeec6fd5
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-01 16:18:00 +0900

    new: solve ex 5

---
+++ easy1/4.rb
@@ -0,0 +1,37 @@
+# def count_occurrences(items)
+#   result = {}
+#   items.each do |item|
+#   if result[item].nil?
+#     result[item] = 1
+#   else
+#     result[item] += 1
+#   end
+#   end
+#   result
+# end
+
+def count_occurrences(items)
+  result = {}
+  items.each do |item|
+    result[item].nil? ? result[item] = 1 : result[item] += 1
+  end
+
+  result.each do |k, v|
+    puts "#{k} => #{v}"
+  end
+end
+
+
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+count_occurrences(vehicles)
+
+# Expected output
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
