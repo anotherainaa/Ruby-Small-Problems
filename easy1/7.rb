011e962a71179263c5dbc444a1d60e4c76d37385
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-01 17:07:28 +0900

    solve ex 7

---
+++ easy1/7.rb
@@ -0,0 +1,12 @@
+def stringy(integer)
+  string = ''
+  integer.times do |i|
+   i.even? ? string << '1' : string << '0'
+  end
+  string
+end
+
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
