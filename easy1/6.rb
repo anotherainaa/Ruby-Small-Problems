f1fd9ceb056296c616adb357fb2c07c9c405e26b
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-01 16:52:56 +0900

    comitting unsaved changes

--- easy1/6.rb
+++ easy1/6.rb
@@ -13,3 +13,5 @@ end
 puts reverse_words('Professional')          # => lanoisseforP
 puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
 puts reverse_words('Launch School')         # => hcnuaL loohcS
+
+
