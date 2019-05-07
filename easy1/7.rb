f68d6af989ce79ba46efbe12c1b300302e6744cb
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-07 16:27:13 +0900

    exercise 7 further exploration

--- easy1/7.rb
+++ easy1/7.rb
@@ -1,12 +1,28 @@
-def stringy(integer)
+# def stringy(integer)
+#   string = ''
+#   integer.times do |i|
+#    i.even? ? string << '1' : string << '0'
+#   end
+#   string
+# end
+
+# Further exploration
+
+def stringy(integer, start_value = 1)
   string = ''
   integer.times do |i|
-   i.even? ? string << '1' : string << '0'
+    if start_value == 0
+      i.even? ? string << '0' : string << '1'
+    else
+      i.even? ? string << '1' : string << '0'
+    end
   end
   string
 end
+
 
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+puts stringy(7, 0) == '0101010'
