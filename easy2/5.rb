8f56fb64cf49902d4f417a399e72182bcc3e8399
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-12 17:50:07 +0900

    solve easy2 ex 5

---
+++ easy2/5.rb
@@ -0,0 +1,20 @@
+# puts "What is your name?"
+# name = gets.chomp
+
+# if name.include?('!')
+#   puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+# else
+#   puts "Hello #{name}."
+# end
+
+# Solution
+
+puts "What is your name?"
+name = gets.chomp
+
+if name[-1] == '!'
+  name = name.chop
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
