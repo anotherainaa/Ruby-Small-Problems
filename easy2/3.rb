3e1a1a6011fa15c36e1ad1d207c2cc7330aa042f
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-12 17:30:19 +0900

    add comment for further exploration

--- easy2/3.rb
+++ easy2/3.rb
@@ -7,5 +7,7 @@ percentage = gets.chomp.to_f
 total_tip = percentage / 100 * bill.round(2)
 total_paid = bill + total_tip.round(2)
 
+# Further exploration
+
 puts "The tip is $#{format('%02.2f', total_tip)}"
 puts "The total is $#{format('%02.2f',total_paid)}"
