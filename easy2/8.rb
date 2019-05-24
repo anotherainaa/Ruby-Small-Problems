ac6b152006a1e1afb1eb90dfa27bda0e35ab8b61
Ainaa Azman <ainaasakinah@gmail.com>
2019-05-24 14:42:18 +0900

    define method for sum and product in exercise 8

---
+++ easy2/8.rb
@@ -0,0 +1,39 @@
+def compute_sum(number)
+  sum = nil
+  for i in 1..number
+    if sum.nil?
+      sum = i
+    else
+      sum += i
+    end
+  end
+  sum
+end
+
+def compute_product(number)
+  product = nil
+  i = 1
+  while i <= number
+    if product.nil?
+      product = i
+    else
+      product *= i
+    end
+    i += 1
+  end
+  product
+end
+
+puts ">> Please enter an integer greater than 0:"
+number = gets.chomp.to_i
+
+puts ">> Enter 's' to compute the sum, 'p' to compute the product."
+input = gets.chomp
+
+if input == 's'
+  puts "The sum of the integers between 1 and #{number} is #{compute_sum(number)}."
+elsif input == 'p'
+  puts "The product of the integers between 1 and #{number} is #{compute_product(number)}."
+else
+  puts "Unknown operation."
+end
