897f9a9f55d3d80786d8f35c048b20091be00c71
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-02 21:44:44 +0900

    revise necessity of argument on #split

--- easy1/5_reverse_it.rb
+++ easy1/5_reverse_it.rb
@@ -1,7 +1,7 @@
 # Solution 1: Ruby idiomatic way
 
 def reverse_sentence(string)
-  string.split(' ').reverse.join(' ')
+  string.split.reverse.join(' ')
 end
 
 # Solution 2: Iterate over array and return last
@@ -11,19 +11,19 @@ end
 
 def reverse_sentence2(string)
   new_string = []
-  strings_array = string.split(' ')
+  strings_array = string.split
   until strings_array.length <= 0
     new_string << strings_array.pop
   end
   new_string.join(' ')
 end
 
-# Solution 3: Using loop 
+# Solution 3: Using loop
 # Mutating an array while looping through it
 
 def reverse_sentence3(string)
   new_string = []
-  strings_array = string.split(' ')
+  strings_array = string.split
   count = strings_array.length
   loop do
     new_string << strings_array.pop
@@ -33,9 +33,9 @@ def reverse_sentence3(string)
   new_string.join(' ')
 end
 
-p reverse_sentence3('')
-p reverse_sentence3('Hello World')
-p reverse_sentence3('Reverse these words')
-puts reverse_sentence3('') == ''
-puts reverse_sentence3('Hello World') == 'World Hello'
-puts reverse_sentence3('Reverse these words') == 'words these Reverse'
+p reverse_sentence('')
+p reverse_sentence('Hello World')
+p reverse_sentence('Reverse these words')
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
