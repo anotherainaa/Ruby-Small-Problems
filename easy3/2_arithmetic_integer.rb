bc7ebde625dc501674bf6e0e8b4ff34012d1e4f0
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-10 19:55:56 +0900

    revision easy3 ex2

--- easy3/2_arithmetic_integer.rb
+++ easy3/2_arithmetic_integer.rb
@@ -1,3 +1,9 @@
+
+# notes: edge cases to consider are not having 0 for the second number
+# if we do, it will throw an error at / because cannot be divided by 0
+# Note that only operators that require division throws an error like #/ and #%
+# Other methods will return the appropriate result accordingly
+
 puts "Enter the first number:"
 first_number = gets.chomp.to_i
 puts "Enter the second number:"
