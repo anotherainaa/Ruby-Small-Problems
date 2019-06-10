ea2ffe52b403636f730dbc93957c2c99a3d9e524
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-10 18:41:21 +0900

    add further exploration easy2 ex5

--- easy2/5_greeting_a_user.rb
+++ easy2/5_greeting_a_user.rb
@@ -22,3 +22,17 @@ if name[-1] == '!'
 else
   puts "Hello #{name}."
 end
+
+# Further exploration
+
+
+puts "What is your name?"
+name = gets.chomp
+
+if name[-1] == '!'
+  name.chop!
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
+
