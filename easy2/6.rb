c1fc8df6cdea18f6f976668a3655a8baa4a9868e
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-10 18:45:39 +0900

    revision easy2 ex6

--- easy2/6.rb
+++ easy2/6.rb
@@ -1,11 +1,11 @@
 # Print all odd numbers from 1 to 99, inclusive. All numbers should be printed on
 # separate lines.
 
-# for i in 1..99
-#   if i % 2 != 0
-#     puts i
-#   end
-# end
+for i in 1..99
+  if i % 2 != 0
+    puts i
+  end
+end
 
 # Further exploration
 
@@ -15,11 +15,11 @@
 
 # Array#select + Integer#odd method
 
-# array = (1..99).to_a
+array = (1..99).to_a
 
-# array.select do |element|
-#   puts e if e.odd?
-# end
+array.select do |element|
+  puts e if e.odd?
+end
 
 # OR
 
@@ -28,3 +28,18 @@
 # Integer#upto
 
 1.upto(99) {|i| puts i if i.odd? }
+
+# Other solution 
+
+(1..99).each do |n|
+  puts n if n.odd?
+end
+
+# LS Solution
+# Note: This solution is nice because there are less times to iterate through. 
+
+value = 1
+while value <= 99
+  puts value
+  value += 2
+end
