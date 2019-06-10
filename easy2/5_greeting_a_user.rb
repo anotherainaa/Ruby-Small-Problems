b56293a990ff4a86e3aa74bf200ed1fddb7bbd7b
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-10 18:27:00 +0900

    revision easy2 ex5

---
+++ easy2/5_greeting_a_user.rb
@@ -0,0 +1,24 @@
+# My solution
+
+puts "What is your name?"
+name = gets.chomp
+
+if name.include?('!')
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
+
+# LS Solution 
+# Note : #chop return the chopped string , not the chopped item
+# You can use #last on an array but there is no #last method on string. 
+
+puts "What is your name?"
+name = gets.chomp
+
+if name[-1] == '!'
+  name = name.chop
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
