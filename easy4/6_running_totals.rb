f1b341c90d8038cfeffad8364568ac6882ece09e
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-13 08:43:56 +0900

    refactor easy4 ex6

--- easy4/6_running_totals.rb
+++ easy4/6_running_totals.rb
@@ -10,8 +10,7 @@ end
 def running_total2(array)
   sum = 0
   array.each_with_object([]) do |value, array|
-    sum += value
-    array << sum
+    array << sum += value
   end
 end
 
@@ -20,14 +19,13 @@ end
 def running_total3(array)
   new_array = []
   array.inject(0) do |sum, value|
-    sum += value
-    new_array << sum
+    new_array << sum += value
     sum
   end 
   new_array
 end
 
-p running_total2([2, 5, 13]) == [2, 7, 20]
-p running_total2([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-p running_total2([3]) == [3]
-p running_total2([]) == []
+p running_total3([2, 5, 13]) == [2, 7, 20]
+p running_total3([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+p running_total3([3]) == [3]
+p running_total3([]) == []
