fd86fd96b6b8d605d3b3346932fa19e87552cb12
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-22 14:21:48 +0900

    revision easy4 ex 5

--- easy4/5_multiples_of_3_and_5.rb
+++ easy4/5_multiples_of_3_and_5.rb
@@ -29,6 +29,10 @@ end
 
 # Further exploration
 # it was really hard to get working as I didn't know where nil was coming from initially
+# inject is easier to use when paired with another method like select
+# but as it is, a developer would really have to know what it works and what it returns
+# to understand what the code below is doing. 
+
 def multisum3(max_value)
   (1..max_value).reduce(0) do |sum, number|
     if multiple?(number, 3) || multiple?(number, 5)
@@ -39,8 +43,27 @@ def multisum3(max_value)
   end
 end
 
+# For example, below is a revision solution attempted with PEDAC and a basic loop construct
+# I found that this is was easier to check whether I was on the right path
+# by capturing all the numbers I want in the array result first
+# and calling method inject after it to sum the numbers
 
-p multisum3(3) == 3
-# p multisum3(5) == 8
+def multisum4(number)
+  result = []
+  counter = 1
+
+  loop do
+    if multiple?(counter, 3) || multiple?(counter, 5)
+      result << counter
+    end
+
+    counter += 1
+    break if counter > number
+  end
+  result.inject(:+)
+end
+
+p multisum4(3) == 3
+p multisum4(5) == 8
 # p multisum3(10) == 33
 # p multisum3(1000) == 234168
