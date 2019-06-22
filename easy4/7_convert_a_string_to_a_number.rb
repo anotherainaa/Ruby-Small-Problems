003063cc00094c5fe688d7610a75ee885d858fe1
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-22 18:10:42 +0900

    add further exploraion easy4 ex 7

--- easy4/7_convert_a_string_to_a_number.rb
+++ easy4/7_convert_a_string_to_a_number.rb
@@ -8,7 +8,13 @@ NUMBER = {
   '6' => 6,
   '7' => 7,
   '8' => 8,
-  '9' => 9
+  '9' => 9,
+  'A' => 10, 
+  'B' => 11, 
+  'C' => 12,
+  'D' => 13,
+  'E' => 14,
+  'F' => 15
 }
 
 def string_to_integer(string)
@@ -22,5 +28,17 @@ def string_to_integer(string)
   integer
 end
 
+#Further exploration
+
+def hexadecimal_to_integer(string)
+  numbers = string.chars.map do |element|
+    NUMBER[element.upcase]
+  end
+  value = 0
+  numbers.each {|number| value = 16 * value + number}
+  value
+end
+
 p string_to_integer('4321')  == 4321
 p string_to_integer('570') == 570
+p hexadecimal_to_integer('4D9f') == 19871
