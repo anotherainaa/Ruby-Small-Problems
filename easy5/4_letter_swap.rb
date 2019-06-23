f25507f245eea2f17cb33761af0aeaf2d8c8bb1f
Ainaa Azman <ainaasakinah@gmail.com>
2019-06-23 19:41:46 +0900

    revision easy5 ex 4

--- easy5/4_letter_swap.rb
+++ easy5/4_letter_swap.rb
@@ -1,13 +1,28 @@
 require 'pry'
 
 def swap(string)
-  words = string.split(' ').map do |word|
+  words = string.split.map do |word|
     first = word[0]
     word[0] = word[-1]
     word[-1] = first
     word
   end
   words.join(' ')
+end
+
+# LS Solution
+# Refactor using a helper method and parallel assignment
+
+def swap_first_last_character(word)
+  word[0], word[-1] = word[-1], word[0]
+  word
+end
+
+def swap(words)
+  words.split.map do |word|
+    swap_first_last_character(word)
+  end
+  .join(' ')
 end
 
 p swap('Oh what a wonderful day it is')  == 'hO thaw a londerfuw yad ti si'
