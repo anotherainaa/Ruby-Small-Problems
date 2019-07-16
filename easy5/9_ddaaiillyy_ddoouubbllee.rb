f7b2a4e6c65c854308b6ad97891140e286f41b87
Ainaa Azman <ainaasakinah@gmail.com>
2019-07-16 23:59:13 +0900

    add revision notes to easy5 ex 9

--- easy5/9_ddaaiillyy_ddoouubbllee.rb
+++ easy5/9_ddaaiillyy_ddoouubbllee.rb
@@ -1,10 +1,10 @@
 def crunch(string)
   result = []
   words = string.split('')
-  
+
   saved_letter = words.shift
   result << saved_letter
-  
+
   words.each do |word|
     if word == " "
       result << word
@@ -16,6 +16,18 @@ def crunch(string)
   result.join
 end
 
+=begin
+The second time around, I tried to break down the algorith the way LS provided
+the solution.
+
+After reading the further exploration part, these are revision that I'd like
+to try the next revision around :
+- Using #each_with_object
+- refactoring my original solution which should probably be like this:
+Insert the character if it is not the same with the last inserted character
+- Find regex solution to this
+
+=end
 
 p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
 p crunch('4444abcabccba') == '4abcabcba'
