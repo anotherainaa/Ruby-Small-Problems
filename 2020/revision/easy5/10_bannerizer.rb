e4b2c82fb3858ed3a8b3b689d6b9a1b931495794
Ainaa Azman <ainaasakinah@gmail.com>
2020-11-02 16:39:22 +0900

    new: First attempt at Further exploration

--- 2020/revision/easy5/10_bannerizer.rb
+++ 2020/revision/easy5/10_bannerizer.rb
@@ -56,6 +56,83 @@ def print_in_box(string)
   puts outer_border
 end
 
-print_in_box("test")
-print_in_box("To boldly go where no one has gone before.")
-print_in_box("")
+# print_in_box("test")
+# print_in_box("To boldly go where no one has gone before.")
+# print_in_box("")
+
+
+
+=begin
+Further exploration. 
+Modify this method so it will truncate the message if it will be too wide to fit inside a standard terminal window (80 columns, including the sides of the box). 
+For a real challenge, try word wrapping very long messages so they appear on multiple lines, but still within a box.
+
+Problem
+- Given a string of text, print it in a box 
+
+Rules
+- If it's more than 80 columns, break it up so that they still fit within the box. 
+- Extend the box to print out all the string. 
+
+Input: String, more than 80 columns 
+Output: String printed in a neatly formatted box. 
+
+Example: 
+
++------------------------------------------------------------------------------+
+|                                                                              |
+| To boldly go where no one has gone before. To boldly go where no one has gon |
+| e before. To boldly go where no one has gone before. To boldly go where no o |
+| ne has gone before.                                                          |
+|                                                                              |
++------------------------------------------------------------------------------+
+
+- Note that it has to be 76 characters + 2 spaces + 2 borders to create 80. 
+- The message has to be wrapped as many times until it finishes.  
+
+# print_in_box("To boldly go where no one has gone before. To boldly go where no one has gone before. To boldly go where no one has gone before. To boldly go where no one has gone before.")
+
+# Data strcuture
+- Array to split up the string? 
+
+# Algorithm 
+- check if the string has more than 76 characters. 
+- if yes, print until message is finished. 
+- else print normal 
+
+=end
+
+def print_in_box(string)  
+  outer_border = "+" + "-" * (string.length + 2) + "+"
+  inner_border = "|" + " " * (string.length + 2) + "|"
+  middle = "| " + string  + " |"
+
+  if string.length <= 76
+    puts outer_border
+    puts inner_border
+    puts middle
+    puts inner_border
+    puts outer_border
+  else
+    messages = string.scan(/.{1,76}/m)
+
+    puts "+" + "-" * (messages[0].length + 2) + "+"
+    puts "|" + " " * (messages[0].length + 2) + "|"
+
+    messages.each do |message|
+      # puts message
+      puts "| " + message + " " * (76 - message.length) + " |"
+    end
+
+    puts "|" + " " * (messages[0].length + 2) + "|"
+    puts "+" + "-" * (messages[0].length + 2) + "+"
+  end
+end
+
+
+# print_in_box("test")
+print_in_box("To boldly go where no one has gone before. To boldly go where no one has gone before. To boldly go where no one has gone before. To boldly go where no one has gone before.")
+
+
+# Final comments
+# How can I change the method to be more DRY?  
