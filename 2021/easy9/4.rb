cc6827d14cc6dd28364398664a99fb0130404d72
Ainaa Azman <ainaasakinah@gmail.com>
2021-01-21 12:01:02 +0900

    add easy 9 q4 and q5

--- 2021/easy9/4.rb
+++ 2021/easy9/4.rb
@@ -3,15 +3,18 @@ Further exploration
 =end
 
 
+# def sequence(int)
+#   results = []
+#   for i in 1..int
+#     results << i
+#   end
+
+#   results
+# end
+
 def sequence(int)
-  results = []
-  for i in 1..int
-    results << i
-  end
-
-  results
+  (1..int).to_a
 end
-
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
 p sequence(1) == [1]
