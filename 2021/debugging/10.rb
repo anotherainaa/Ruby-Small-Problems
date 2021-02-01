ad327672c2a65e2b61810858699ceae0a611c8f4
Ainaa Azman <ainaasakinah@gmail.com>
2021-02-01 13:07:42 +0900

    add: further exploration q10

--- 2021/debugging/10.rb
+++ 2021/debugging/10.rb
@@ -22,7 +22,8 @@ end
 def idf(term, documents)
   number_of_documents = documents.length
   number_of_documents_with_term = documents.count { |d| tf(term, d) > 0 }
-  Math.log(number_of_documents.to_f / number_of_documents_with_term)
+  idf = Math.log(number_of_documents.to_f / number_of_documents_with_term)
+  idf.infinite? ? 0.0 :idf
 end
 
 # Very simple example
