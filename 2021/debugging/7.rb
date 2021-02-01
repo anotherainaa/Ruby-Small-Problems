d4ecf713e487ebc5eef55b3105f89cfcb0448101
Ainaa Azman <ainaasakinah@gmail.com>
2021-02-01 11:45:15 +0900

    add: debugging problems

---
+++ 2021/debugging/7.rb
@@ -0,0 +1,31 @@
+# def neutralize(sentence)
+#   words = sentence.split(' ')
+#   words.dup.each do |word|
+#     words.delete(word) if negative?(word)
+#   end
+
+#   words.join(' ')
+# end
+
+def neutralize(sentence)
+    words = sentence.split(' ')
+    words.reject! do |word|
+      negative?(word)
+    end
+  
+    words.join(' ')
+  end
+
+def negative?(word)
+  [ 'dull',
+    'boring',
+    'annoying',
+    'chaotic'
+  ].include?(word)
+end
+
+puts neutralize('These dull boring cards are part of a chaotic board game.')
+# Expected: These cards are part of a board game.
+# Actual: These boring cards are part of a board game.
+
+# The alternative is to use reject!!!! 
